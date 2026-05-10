bfc0c737a7ad21d3c1c25cc97ae6e8cc3c42eeb2
mistalyon <kemaloglu100@gmail.com>
2026-05-10 04:11:32 +0300

    Update trailingSlash configuration fix(seo): trailingSlash always for canonical URL consistencyin astro.config.mjs
    Changed trailingSlash configuration from 'never' to 'always'.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://malatyareklam.com',
-  trailingSlash: 'never',
+  trailingSlash: 'always',
   build: {
     format: 'directory',
   },
